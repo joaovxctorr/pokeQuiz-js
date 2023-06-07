69bee94a6793d0fec64c948758b5f05e65f8b5bd
João Victor <83352721+joaovxctorr@users.noreply.github.com>
2023-06-07 20:17:21 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Quantos Pokemons tem na primeira geração?",
+      answers: [
+        { option: "151", correct: true },
+        { option: "95", correct: false },
+        { option: "202", correct: false },
+      ],
+    },
+
+    {
+      question: "Qual a maior fraqueza do tipo Dragão?",
+      answers: [
+        { option: "Fogo", correct: false },
+        { option: "Fada", correct: true },
+        { option: "Psiquico", correct: false },
+      ],
+    },
+
+    {
+      question: "Qual desses é lendario?",
+      answers: [
+        { option: "Pikachu", correct: false },
+        { option: "Kyurem", correct: true },
+        { option: "Lucario", correct: false },
+      ],
+    },
+
+    {
+      question: "Quem é o melhor companheiro de Ash?",
+      answers: [
+        { option: "Charmander", correct: false },
+        { option: "Eevee", correct: false },
+        { option: "Pikachu", correct: true },
+      ],
+    },
+
+    {
+      question: "Precisa de quantas insignias para entrar na Liga Pokemon?",
+      answers: [
+        { option: "7", correct: false },
+        { option: "12", correct: false },
+        { option: "8", correct: true },
+      ],
+    },
+
+    {
+      question: "Qual a evolução do Magikarp?",
+      answers: [
+        { option: "Dragonite", correct: false },
+        { option: "Gyarados", correct: true },
+        { option: "Magikarptron", correct: false },
+      ],
+    },
+
+    {
+      question: "Quais dessas são evoluções do Eevee?",
+      answers: [
+        { option: "Flareon, Dragoneon, Glaceon", correct: false },
+        { option: "Vaporeon, Steeleon, Sylveon", correct: false },
+        { option: "Glaceon, Sylveon e Vaporeon", correct: true },
+      ],
+    },
+
+    {
+      question: "Qual o tipo do Gyarados?",
+      answers: [
+        { option: "Agua e Dragao", correct: false },
+        { option: "Agua", correct: false },
+        { option: "Agua e Voador", correct: true },
+      ],
+    },
+
+    {
+      question: "Quem foi o primeiro rival de Ash?",
+      answers: [
+        { option: "Gary", correct: true },
+        { option: "Brook", correct: false },
+        { option: "Paul", correct: false },
+      ],
+    },
+
+    {
+      question: "Quem é a dupla iconica que sempre tenta roubar o Pikachu do Ash?",
+      answers: [
+        { option: "Josh e Janice", correct: false },
+        { option: "James e Jessie", correct: true },
+        { option: "John e Jessica", correct: false },
+      ],
+    },
+
+  ];
